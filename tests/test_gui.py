fdca12be6a0f9bdc7757c771aefe763fb117ee5a
Thomas Michelat <thomas.michelat@xfel.eu>
2024-09-09 12:12:32 +0200

    h

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -50,16 +50,16 @@ def test_connect_to_kafka(mock_db, qtbot):
 
         win.close()
 
-    # with patch(f"{pkg}.KafkaConsumer") as kafka_cns, \
-    #      patch(f"{pkg}.KafkaProducer") as kafka_prd:
-    #     win = MainWindow(db_dir, True).close()
+    with patch(f"{pkg}.KafkaConsumer") as kafka_cns, \
+         patch(f"{pkg}.KafkaProducer") as kafka_prd:
+        win = MainWindow(db_dir, True).close()
 
-    #     kafka_cns.assert_called_once()
-    #     kafka_prd.assert_called_once()
+        with qtbot.waitSignal(win._editor.check_result):
+            kafka_cns.assert_called_once()
+            kafka_prd.assert_called_once()
 
-    #     qtbot.addWidget(win)
-    #     qtbot.waitSignal(win._editor.check_result)
-    #     win.close()
+        win.close()
+
 
 @pytest.mark.skip
 def test_editor(mock_db, mock_ctx, qtbot):
